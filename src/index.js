6df1feaef1fffdd76dc68dc1b7aad1d6eefb6c8b
Yana Sokurenko <yanamoss@gmail.com>
2021-09-05 13:43:51 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const element = React.createElement('div', {
+  a: 12,
+  b: 4,
+  children: ['Hello', ' ', 'world'],
+});
+console.log(element);
+
+ReactDOM.render(element, document.querySelector('#root'));
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
