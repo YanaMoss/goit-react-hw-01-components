76972f5eeb71dee1830e1b51abbc2b09036268f1
Yana Sokurenko <yanamoss@gmail.com>
2021-09-13 16:51:06 +0300

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import React from "react";
-import "./App.css";
-import Profile from "./components/profile/Profile";
-import user from "./user.json";
-import statisticalData from "./statistical-data.json";
-import Statistics from "./components/statistics/Statistics";
-import FriendsList from "./components/friends/FriendsList";
-import friends from "./friends.json";
-import TransactionList from "./components/transactions/TransactionList";
-import transactions from "./transactions.json";
-
+import React from 'react';
+import './App.css';
+import Profile from './components/profile/Profile';
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+import Statistics from './components/statistics/Statistics';
+import FriendsList from './components/friends/FriendsList';
+import friends from './data/friends.json';
+import TransactionList from './components/transactions/TransactionList';
+import transactions from './data/transactions.json';
 
 const App = () => {
-   return (<div className="App">
+  return (
+    <div className="App">
       <Profile
-      key = {user.id}
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
+        key={user.id}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
       />
-      <Statistics title={"Upload stats"} stats={statisticalData} />
+      <Statistics title={'Upload stats'} stats={statisticalData} />
       <FriendsList friends={friends} />
-      <TransactionList transaction={transactions}/> 
-   </div>
-      )}
+      <TransactionList transaction={transactions} />
+    </div>
+  );
+};
 
 export default App;
